da448f7022f6407e29ef1383da76ee65de605338
clancy.meade <clancy.meade@mu-c1-16.mullen.pvt.k12.co.us>
2020-02-14 09:21:17 -0700

    Commit

--- Dive Sighting Database/src/DiveSightingDriver.java
+++ Dive Sighting Database/src/DiveSightingDriver.java
@@ -16,6 +16,11 @@ public class DiveSightingDriver {
 				+ ", please choose from a dive sighting below, enter the sighting type for more information");
 
 		System.out.println();
+		
+boolean keepViewing = true;  
+		
+		while(keepViewing)
+			{
 
 		Sighting[] sightings = new Sighting[4];
 		{
@@ -29,19 +34,26 @@ public class DiveSightingDriver {
 			s.displaySightingType();
 		}
 
+		
+		
 		String diveSightingChoice = userInput.nextLine();
 
 		System.out.println("Great, here is some more information.");
-
+		
+		
 		if (diveSightingChoice.toUpperCase().equals("ANIMAL SIGHTING")) {
 
 			System.out.println();
 			AnimalSighting firstSighting = new AnimalSighting();
 			
 			firstSighting.displayNumericalData();
+			userInput.nextLine(); 
 			firstSighting.displayDiveSite();
+			userInput.nextLine();
 			firstSighting.displaySpecies();
+			userInput.nextLine();
 			firstSighting.displayColor();
+			userInput.nextLine();
 			firstSighting.displayFoodSource();
 		}
 
@@ -52,10 +64,15 @@ public class DiveSightingDriver {
 			ReefSighting secondSighting = new ReefSighting();
 			
 			secondSighting.displayNumericalData();
+			userInput.nextLine();
 			secondSighting.displayDiveSite();
+			userInput.nextLine();
 			secondSighting.displaySpecies();
+			userInput.nextLine();
 			secondSighting.displayColor();
+			userInput.nextLine();
 			secondSighting.displayArea();
+			userInput.nextLine();
 			secondSighting.displayShape();
 		}
 
@@ -65,9 +82,13 @@ public class DiveSightingDriver {
 			WreckSighting thirdSighting = new WreckSighting();
 
 			thirdSighting.displayNumericalData();
+			userInput.nextLine();
 			thirdSighting.displayDiveSite();
+			userInput.nextLine();
 			thirdSighting.displayDistanceFromCoast();
+			userInput.nextLine();
 			thirdSighting.displaySeaLife();
+			userInput.nextLine();
 			thirdSighting.displayVolume();
 		}
 
@@ -77,12 +98,37 @@ public class DiveSightingDriver {
 			TreasureSighting fourthSighting = new TreasureSighting();
 			
 			fourthSighting.displayNumericalData();
+			userInput.nextLine();
 			fourthSighting.displayDiveSite();
+			userInput.nextLine();
 			fourthSighting.displayDistanceFromCoast();
+			userInput.nextLine();
 			fourthSighting.displaySeaLife();
+			userInput.nextLine();
 			fourthSighting.displayValueSource();
 
 		}
+		
+		System.out.println();
+		
+		System.out.println("Would you like to view a different dive sighting? \n(1) Yes \n(2) No");
+		
+		Scanner intInput = new Scanner(System.in); 
+		
+		int keepSightingNum = intInput.nextInt(); 
+		
+		if(keepSightingNum == 1)
+			{ 
+				keepViewing = true; 
+			}
+		else if(keepSightingNum == 2)
+			{
+				keepViewing = false; 
+				System.out.println("Have a great day!");
+			}
+	
+		
+			}
 
 	}
 
